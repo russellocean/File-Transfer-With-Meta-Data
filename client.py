6db895eb9f5cfd1b265b4f1d4decb0df058272a2
russellocean <russellwelch17@gmail.com>
2024-02-23 11:43:53 -0500

    Implement start

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
+import logging
+import os
+import sys
+import time
 from socket import *
-from struct import pack, unpack
-import os, time, sys, logging, hashlib
 
 
 class FileTransferClient:
@@ -21,7 +23,6 @@ class FileTransferClient:
         self.client_socket = None
         self.hash_length = hash_length
 
-
     def start(self):
         """
         Initiates a connection to the server and marks the client as running. This function
@@ -33,7 +34,7 @@ class FileTransferClient:
         self.is_running attribute to True upon a successful connection. It logs
         the connection attempt and reports any errors encountered during the process.
 
-        Returns: 
+        Returns:
             True or false depending on whether the connection was successful.
 
         Exceptions:
@@ -51,9 +52,16 @@ class FileTransferClient:
             - Ensure that the server is up and running at the specified host and port
               before calling this method.
         """
-        # TODO: Implement the functionality described in this function's docstring
-        pass
-
+        try:
+            self.client_socket = socket(AF_INET, SOCK_STREAM)
+            self.client_socket.connect((self.host, self.port))
+            self.logger.info(f"Connected to server at {self.host}:{self.port}")
+            return True
+        except OSError as e:
+            self.logger.error(
+                f"Failed to connect to server at {self.host}:{self.port}. Error: {e}"
+            )
+            return False
 
     def read_file(self, filepath):
         """
@@ -65,21 +73,20 @@ class FileTransferClient:
         with open(filepath, "rb") as file:
             data = file.read()
             return data
-        
 
     def send_file(self, filepath):
         """
         Sends a file to the server using the established connection.
 
         File Transfer Protocol:
-        1. Pack the metadata into a bytes object using the format below, up to but not including 
+        1. Pack the metadata into a bytes object using the format below, up to but not including
            the file data.
         2. Concatenate the file data to the metadata bytes object (e.g. metadata + file_data).
-        3. Compute the hash of the metadata + file data and append it to the bytes object 
+        3. Compute the hash of the metadata + file data and append it to the bytes object
            containing the metadata and the file data (metadata + file data + hash).
         4. Send the bytes object (i.e. metadata + file data + hash) to the server.
         5. Wait for a response from the server containing the hash of the data the server received.
-        6. If the server's hash matches the hash of the data sent, the file was sent successfully. 
+        6. If the server's hash matches the hash of the data sent, the file was sent successfully.
            Return whether the file was sent successfully or not.
 
         Parameters:
@@ -87,14 +94,14 @@ class FileTransferClient:
 
         Returns:
             bool: True if the file was sent successfully, False otherwise.
-        
+
         Exceptions:
             - TimeoutError: Raised if the socket times out while waiting for a response.
             - ConnectionResetError: Raised if the server resets the connection.
             - ConnectionAbortedError: Raised if the server aborts the connection.
             - OSError: Raised if the socket encounters a problem (e.g., network
               issue, invalid host/port).
-              
+
         Usage Example:
             client = FileTransferClient("localhost", 5000)
             if client.start():
@@ -102,7 +109,6 @@ class FileTransferClient:
         """
         # TODO: Implement the functionality described in this function's docstring
         pass
-
 
     def encode_metadata(self, filepath):
         """
@@ -143,7 +149,6 @@ class FileTransferClient:
 
         # TODO: Implement the functionality described in this function's docstring
 
-
     def compute_hash(self, data):
         """
         Computes a hash for given data using the SHAKE-128 algorithm.
@@ -151,8 +156,8 @@ class FileTransferClient:
         In this assignment, we'll use the hashlib library to compute a hash of the data
         being shared between the client and the server. A hash is a fixed-length string
         generated based on arbitrary input data. The same data will result in the same
-        hash, and any change to the data will result in a different hash.  We're using this 
-        very simplistically to introduce the idea of hashing and integrity checking, which 
+        hash, and any change to the data will result in a different hash.  We're using this
+        very simplistically to introduce the idea of hashing and integrity checking, which
         we'll be exploring in more detail later in the semester.
 
         1. Import hashlib and call hashlib.shake_128() to create a new SHAKE-128 hash object.
@@ -161,7 +166,7 @@ class FileTransferClient:
 
         The shake_128 algorithm is convenient for us since we can specify the length of the
         hash it produces. This allows us to generate a short hash for use in our tests.
-        
+
         Parameters:
             data (bytes): The data to be hashed.
             hash_length (int): The desired length of the hash output.
@@ -172,14 +177,13 @@ class FileTransferClient:
         # TODO: Implement the functionality described in this function's docstring
         pass
 
-
     def shutdown(self):
         """
         Shuts down the client by stopping its operation and closing the socket.
 
         This method is responsible for cleanly terminating the client's operation.
-        It indicates that the client is no longer in operation. If the client has an 
-        active socket connection, it closes the socket to ensure proper release of 
+        It indicates that the client is no longer in operation. If the client has an
+        active socket connection, it closes the socket to ensure proper release of
         resources and network cleanup.
 
         The method logs the shutdown process, providing visibility into the client's
@@ -200,8 +204,8 @@ class FileTransferClient:
         """
         self.logger.info("Client is shutting down...")
 
-         # TODO: Implement the functionality described in this function's docstring
-        
+        # TODO: Implement the functionality described in this function's docstring
+
         self.logger.removeHandler(self.handler)
 
 
